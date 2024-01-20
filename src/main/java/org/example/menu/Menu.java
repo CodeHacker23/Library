5d7fd9752e96daff514cd1bcc68681e74c1f7bd0
Квантор <ilarion0@yandex.ru>
2024-01-21 00:51:31 +0500

    rename 2 larik

--- src/main/java/org/example/menu/Menu.java
+++ src/main/java/org/example/menu/Menu.java
@@ -36,7 +36,7 @@ public class Menu {
     public void start() {
         initializeBooks();
         while (true) {
-            System.out.println("1 - add book");
+            System.out.println("1 - add book 123");
             System.out.println("2 - print all book");
             System.out.println("3 - вывести список все книг по жанру");
             System.out.println("4 - вывести информацию о книге по названию");
