c91c4a198effa3671a5ef73988666301d0b39923
Nelson2603 <157066286+Nelson2603@users.noreply.github.com>
2024-01-22 02:05:18 +0400

    rename 1 просто обнавил реализовал  5 метод

--- src/main/java/org/example/menu/Menu.java
+++ src/main/java/org/example/menu/Menu.java
@@ -2,34 +2,33 @@ package org.example.menu;
 
 import org.example.entity.Book;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Menu {
     Scanner scanner = new Scanner(System.in);
-    Book[] newBooks = new Book[100];
+    Book[] books = new Book[100];
 
     public void initializeBooks() {
-        newBooks[0] = new Book("Айзек Азимов", "Я, робот", 25.99, 1950, " Научная фантастика");
-        newBooks[1] = new Book("Александр Дюма", "Граф Монте-Кристо", 28.75, 1844, "Приключения");
-        newBooks[2] = new Book("Дж. Р. Р. Толкин", "Властелин колец", 35.50, 1954, "Фэнтези");
-        newBooks[3] = new Book("Харпер Ли", "Убить пересмешника", 18.75, 1960, "Роман");
-        newBooks[4] = new Book("Джордж Оруэлл", "1984", 20.00, 1949, "Дистопия");
-        newBooks[5] = new Book("Фрэнсис Скотт Фицджеральд", "Великий Гэтсби", 22.30, 1925, "Роман");
-        newBooks[6] = new Book("Александр Дюма", "Граф Монте-Кристо", 28.75, 1844, "Приключения");
-        newBooks[7] = new Book("Джейн Остин", "Гордость и предубеждение", 19.90, 1813, "Роман");
-        newBooks[8] = new Book("Эрих Мария Ремарк", "Три товарища", 24.50, 1936, "Роман");
-        newBooks[9] = new Book("Агата Кристи", "Десять негритят", 15.80, 1939, "Детектив");
-        newBooks[10] = new Book("Рэй Брэдбери", "451 градус по Фаренгейту", 17.25, 1953, "Научная фантастика");
-        newBooks[11] = new Book("Лев Толстой", "Война и мир", 29.99, 1869, "Роман");
-        newBooks[12] = new Book("Дж. К. Роулинг", "Гарри Поттер и философский камень", 23.45, 1997, "Фэнтези");
-        newBooks[13] = new Book("Марк Твен", "Приключения Тома Сойера", 16.80, 1876, "Приключения");
-        newBooks[14] = new Book("Джордж Мартин", "Игра престолов", 30.20, 1996, "Фэнтези");
-        newBooks[15] = new Book("Джеймс Гослинг", "Философия Java", 1200.00, 2008, " Техническая литература");
-        newBooks[16] = new Book("Иван Порубель", "Java. Методы программирования", 850.00, 2015, "Обучение");
-        newBooks[17] = new Book("Джошуа Блох", " Java. Эффективное программирование", 1150.00, 2014, " Техническая литература");
-        newBooks[18] = new Book("Игорь Блинов", "Основы Java", 950.00, 2020, "Обучение");
-        newBooks[19] = new Book("Александр Зыбин", "Java. Методы программирования. 50 уроков", 1050.00, 2021, "Техническая литература");
+        books[0] = new Book("Айзек Азимов", "Я, робот", 25.99, 1950, " Научная фантастика");
+        books[1] = new Book("Александр Дюма", "Граф Монте-Кристо", 28.75, 1844, "Приключения");
+        books[2] = new Book("Дж. Р. Р. Толкин", "Властелин колец", 35.50, 1954, "Фэнтези");
+        books[3] = new Book("Харпер Ли", "Убить пересмешника", 18.75, 1960, "Роман");
+        books[4] = new Book("Джордж Оруэлл", "1984", 20.00, 1949, "Дистопия");
+        books[5] = new Book("Фрэнсис Скотт Фицджеральд", "Великий Гэтсби", 22.30, 1925, "Роман");
+        books[6] = new Book("Александр Дюма", "Граф Монте-Кристо", 28.75, 1844, "Приключения");
+        books[7] = new Book("Джейн Остин", "Гордость и предубеждение", 19.90, 1813, "Роман");
+        books[8] = new Book("Эрих Мария Ремарк", "Три товарища", 24.50, 1936, "Роман");
+        books[9] = new Book("Агата Кристи", "Десять негритят", 15.80, 1939, "Детектив");
+        books[10] = new Book("Рэй Брэдбери", "451 градус по Фаренгейту", 17.25, 1953, "Научная фантастика");
+        books[11] = new Book("Лев Толстой", "Война и мир", 29.99, 1869, "Роман");
+        books[12] = new Book("Дж. К. Роулинг", "Гарри Поттер и философский камень", 23.45, 1997, "Фэнтези");
+        books[13] = new Book("Марк Твен", "Приключения Тома Сойера", 16.80, 1876, "Приключения");
+        books[14] = new Book("Джордж Мартин", "Игра престолов", 30.20, 1996, "Фэнтези");
+        books[15] = new Book("Джеймс Гослинг", "Философия Java", 1200.00, 2008, " Техническая литература");
+        books[16] = new Book("Иван Порубель", "Java. Методы программирования", 850.00, 2015, "Обучение");
+        books[17] = new Book("Джошуа Блох", " Java. Эффективное программирование", 1150.00, 2014, " Техническая литература");
+        books[18] = new Book("Игорь Блинов", "Основы Java", 950.00, 2020, "Обучение");
+        books[19] = new Book("Александр Зыбин", "Java. Методы программирования. 50 уроков", 1050.00, 2021, "Техническая литература");
     }
 
 
@@ -61,7 +60,14 @@ public class Menu {
                     // methods4();
                 }
                 case "5" -> {
-//
+                    System.out.println("Вывод книг в порядке возрастании цены нажмите: +" + "" +
+                            "\nВывод книг в порядке убывании цены  нажмите: - ");
+                    answer = scanner.nextLine();
+                    if (answer.equals("+")) {
+                        printPriceBookUp();
+                    } else if (answer.equals("-")) {
+                        printPriceBookDown();
+                    } else System.out.println("НЕ ВЕРНАЯ КОМАНДА!!! ВВЕДИТЕ ЗАНОВО!");
 
 
                 }
@@ -103,9 +109,9 @@ public class Menu {
 
         // Добавляем книгу в первое свободное место в массиве
 
-        for (int i = 0; i < newBooks.length; i++) {
-            if (newBooks[i] == null) {
-                newBooks[i] = newBook;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = newBook;
                 System.out.println("Книга добавлена в библиотеку");
                 return;
             }
@@ -117,7 +123,7 @@ public class Menu {
 
     public void methods2() {
         System.out.println("Список всех книг в библиотеке: ");
-        for (Book book : newBooks) {
+        for (Book book : books) {
             if (book != null) {
                 book.info();
             }
@@ -125,42 +131,65 @@ public class Menu {
     }
 
 
-//
+    public void method5() {
+        for (Book i : books) {
+            if (i != null) {
+                System.out.println("Название книги : " + i.getTitle() + " Цена : " + i.getPrice() + "\n-------------");
+            }
+        }
+    }
+
+    public void printPriceBookDown() {
+        System.out.println("Книги по стоимости в порядке уменьшении цены :");
+        if (books != null) {
+            // Сортировка пузырьком по цене от большей к меньшей
+            for (int i = 0; i < books.length - 1; i++) {
+                for (int j = 0; j < books.length - i - 1; j++) {
+                    if (books[j] != null && books[j + 1] != null &&
+                            books[j].getPrice() < books[j + 1].getPrice()) {
+
+                        Book temp = books[j];
+                        books[j] = books[j + 1];
+                        books[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Вывод названий и цен отсортированных книг
+            for (Book book : books) {
+                if (book != null) {
+                    System.out.println("Название книги : " + book.getTitle() + " , Стоимость  : " + book.getPrice());
+                }
+            }
+        }
+    }
+
+    public void printPriceBookUp() {
+        System.out.println("Книги по стоимости в порядке увеличении цены цены :");
+        if (books != null) {
+            // Сортировка пузырьком по цене от большей к меньшей
+            for (int i = 0; i < books.length - 1; i++) {
+                for (int j = 0; j < books.length - i - 1; j++) {
+                    if (books[j] != null && books[j + 1] != null &&
+                            books[j].getPrice() > books[j + 1].getPrice()) {
+
+                        Book temp = books[j];
+                        books[j] = books[j + 1];
+                        books[j + 1] = temp;
+                    }
+                }
+            }
+
+            // Вывод названий и цен отсортированных книг
+            for (Book book : books) {
+                if (book != null) {
+                    System.out.println("Название книги : " + book.getTitle() + " , Стоимость  : " + book.getPrice());
+                }
+            }
+        }
+    }
 
 }
-
-//    public void methods3 (){
-//        System.out.println("Cписок всех книг по жанру и названию ");
-//        for (int i = 0; i < books.length; i++) {
-//            if(books[i] != null){
-//                System.out.println(books[i].getTitle() +  " " + books[i].getGenre());
-//            }
-//        }
-//    }
-//
-//    public void methods4 (){
-//        String name = scanner.nextLine();
-//        System.out.println("вывести информацию о книге по названию");
-//        System.out.println("=======================================");
-//        for (int i = 0; i < books.length; i++) {
-//            if ( name.equals(books[i].getTitle())) {
-//                System.out.println(books[i].getAuthor() + " " + books[i].getTitle() + " " + books[i].getPrice() + " " + books[i].getYear() + " " + books[i].getGenre());
-//                return;
-//            }
-//        }
-//        System.out.println("Такой книги не существует, попробуйте еще раз! ");
-//    }
-//    public void methods5 (){ //вывести все книги по стоимости");
-//
-//    }
-//    public void methods6 (){
-//
-//    }
-//    public void methods7 (){
-//
-//    }
-
-
 
 
 
